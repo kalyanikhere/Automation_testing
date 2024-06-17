f2e0bc264fff6c9a826c8d35d8c41934c3e19bb6
kalyanikhere <kalyanikhere03@gmail.com>
2024-06-17 20:27:19 +0530

    project added here

---
+++ kalyani_java/src/kalyani_java1/Kalyani2.java
@@ -0,0 +1,14 @@
+package kalyani_java1;
+
+public class Kalyani2 {
+
+	public static void body() {
+		System.out.println("push the project");
+	}
+	
+	public static void main(String[] args) {
+		body();
+
+	}
+
+}
